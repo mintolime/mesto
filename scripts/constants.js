a87754fdd23e0fa641147f62dc373894bda2b9cb
Evgenia Smirnova <evgeniabright@yandex.ru>
2022-11-12 20:01:35 +0700

    refactor function

---
+++ scripts/constants.js
@@ -0,0 +1,18 @@
+const popup = document.querySelector('.popup'); 
+const popupCard = document.querySelector('.popup_add-card');
+const popupProfile = document.querySelector('.popup_edit-profile');
+const popupForm = document.querySelector('.form');
+//BUTTON
+const openPopupBtn = document.querySelector('.button_type_edit');
+const addPopupBtn = document.querySelector('.button_type_add');
+const closePopupBtn = document.querySelector('.button_type_close');
+
+const likeBtn = document.querySelector('.button_type_like');
+//PROFILE
+const profileName = document.querySelector('.profile__name');
+const formInputName = document.querySelector('.form__input_text_name');
+const profileAboutUser = document.querySelector('.form__input_text_about');
+const formAboutUser = document.querySelector('.form__input_text_about');
+//CARD
+const cardInputName = document.querySelector('#nameCard');
+const cardImglink = document.querySelector('#linkCard');
--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,14 @@
-const popup = document.querySelector('.popup');
-const popupForm = document.querySelector('.form');
-const openPopupBtn = document.querySelector('.button_type_edit');
-const closePopupBtn = document.querySelector('.button_type_close');
-const profileName = document.querySelector('.profile__name');
-const formInputName = document.querySelector('.form__input_text_name');
-const profileAboutUser = document.querySelector('.profile__info');
-const formAboutUser = document.querySelector('.form__input_text_about');
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-// сначала объявляем все переменные, затем описываем функции, в самом конце устанавливаем события.
-function openPopup() {
-  popup.classList.add('popup_opened')
+
+function openPopup(element) {
+  element.classList.add('popup_opened')
 };
 
-function closePopup() {
-  popup.classList.remove('popup_opened')
+function closePopup(element) {
+  element.classList.remove('popup_opened')
 };
+
+
+//likeBtn.addEventListener('click', (evt) => evt.target.classList.toggle('button_type_like_active'));
 
 function addValue() {
   formInputName.value = profileName.textContent;
@@ -50,11 +19,12 @@ function changeTextProfile(evt) {
   evt.preventDefault();
   profileName.textContent = formInputName.value;
   profileAboutUser.textContent = formAboutUser.value;
-  closePopup();
+  closePopup(popupProfile);
 };
 
-openPopupBtn.addEventListener('click', openPopup);
-closePopupBtn.addEventListener('click', closePopup);
+addPopupBtn.addEventListener('click', () => openPopup(popupCard));
+openPopupBtn.addEventListener('click', () => openPopup(popupProfile));
+closePopupBtn.addEventListener('click', closePopup(popup));
 openPopupBtn.addEventListener('click', addValue);
 popupForm.addEventListener('submit', changeTextProfile);
 
