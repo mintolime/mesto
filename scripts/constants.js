c39feb038fbc30aba0f8d9a314e49c02c4b4b8fc
Evgenia Smirnova <evgeniabright@yandex.ru>
2022-11-13 19:05:37 +0700

    добавление стилей

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,22 +1,26 @@
 const popup = document.querySelector('.popup');
 const cardContainer = document.querySelector('.cards__list');
 const popupCard = document.querySelector('.popup_add-card');
-const popupProfile = document.querySelector('.popup_edit-profile');
-const popupForm = document.querySelector('.form');
+
 //BUTTON
 const openPopupBtn = document.querySelector('.button_type_edit');
 const addPopupBtn = document.querySelector('.button_type_add');
-const closePopupBtn = document.querySelector('.button_type_close');
-const likeCardBtn = document.querySelector('.button_type_like');
-const deleteCardBtn = document.querySelector('.button_type_delete');
+const closePopupBtns = document.querySelectorAll('.button_type_close');
+//const likeCardBtn = document.querySelector('.button_type_like');
+// const deleteCardBtn = document.querySelector('.button_type_delete');
 //PROFILE
+const popupProfile = document.querySelector('.popup_edit-profile');
+const formProfile = document.querySelector('#formProfile');
 const profileName = document.querySelector('.profile__name'); 
 const formInputName = document.querySelector('.form__input_text_name'); 
 const profileAboutUser = document.querySelector('.profile__info'); 
 const formAboutUser = document.querySelector('.form__input_text_about');
 //CARD
+const formCard = document.querySelector('#formCard');
 const cardInputName = document.querySelector('#nameCard');
 const cardImglink = document.querySelector('#linkCard');
+const imgFigure = document.querySelector('.figure__image');
+const infoFigure = document.querySelector('.figure__info');
 
 const initialCards = [
   {
