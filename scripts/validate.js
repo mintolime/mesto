e3d89d72481cadf083c9191e3ff0ff06ce3e1d52
Evgenia Smirnova <evgeniabright@yandex.ru>
2022-11-27 21:25:01 +0700

    *

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,13 +1,13 @@
 
 
-const enableValidation = {
-  formSelector: '.form',
-  inputSelector: '.form__input',
-  submitButtonSelector: '.button_type_save',
-  inactiveButtonClass: 'button_disabled',
-  inputErrorClass: 'form__input-error',
-  errorClass: 'form__input-error_active',
-};
+// const enableValidation = {
+//   formSelector: '.form',
+//   inputSelector: '.form__input',
+//   submitButtonSelector: '.button_type_save',
+//   inactiveButtonClass: 'button_disabled',
+//   inputErrorClass: 'form__input-error',
+//   errorClass: 'form__input-error_active',
+// };
 
 
 
@@ -20,3 +20,20 @@ const enableValidation = {
 //   errorClass: "`.${inputElement.id}-error`",
 //   errorInput: ".popup__text-input"
 // };
+const formElement = document.querySelector('.form');
+const formInput = formElement.querySelector('.form__input');
+
+// Выбираем элемент ошибки на основе уникального класса
+const formError = formElement.querySelector(`.${formInput.id}-error`);
+console.log(formError)
+const showInputError = (element) => {
+  element.classList.add('form__input_type_error');
+  // Показываем сообщение об ошибке
+  formError.classList.add('form__input-error_active');
+};
+
+const hideInputError = (element) => {
+  element.classList.remove('form__input_type_error');
+  // Скрываем сообщение об ошибке
+  formError.classList.remove('form__input-error_active');
+};
