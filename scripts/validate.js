cab42a44ee4f1a8a8d014aca66afbdb02a2dd180
Evgenia Smirnova <evgeniabright@yandex.ru>
2022-11-28 13:51:16 +0700

    validation form and button

--- scripts/validate.js
+++ scripts/validate.js
@@ -20,20 +20,79 @@
 //   errorClass: "`.${inputElement.id}-error`",
 //   errorInput: ".popup__text-input"
 // };
-const formElement = document.querySelector('.form');
-const formInput = formElement.querySelector('.form__input');
+// const formElement = document.querySelector('.form');
+// const formInput = formElement.querySelector('.form__input');
 
-// Выбираем элемент ошибки на основе уникального класса
-const formError = formElement.querySelector(`.${formInput.id}-error`);
-console.log(formError)
-const showInputError = (element) => {
-  element.classList.add('form__input_type_error');
-  // Показываем сообщение об ошибке
-  formError.classList.add('form__input-error_active');
+// // Выбираем элемент ошибки на основе уникального класса
+// const formError = formElement.querySelector('form__input-error');
+
+const form = document.querySelector('.form');
+const formInput = form.querySelector('.form__input');
+const formError = form.querySelector(`.${formInput.id}-error`);
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('form__input_type_error');
+  inputElement.style.borderBottom = '1px #FF0000 solid'; //изменение цвета валидации
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('form__input-error_active');
 };
 
-const hideInputError = (element) => {
-  element.classList.remove('form__input_type_error');
-  // Скрываем сообщение об ошибке
-  formError.classList.remove('form__input-error_active');
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('form__input_type_error');
+  errorElement.classList.remove('form__input-error_active');
+  errorElement.textContent = '';
 };
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+form.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+});
+
+formInput.addEventListener('input', function () {
+  checkInputValidity(form, formInput);
+});
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid
+  });
+};
+
+const toggleButton = (inputList, buttonElement) =>{
+  if (hasInvalidInput(inputList)){
+    buttonElement.classList.add('button_disabled')
+  }
+else{
+  buttonElement.classList.remove('button_disabled')
+}
+}
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+};
+
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+
+    });
+    setEventListeners(formElement);
+  });
+};
+enableValidation()
