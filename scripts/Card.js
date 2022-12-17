1b2fdd2735a32351ea2193570b4ddc9b21f11fda
mintolime <104442896+mintolime@users.noreply.github.com>
2022-12-17 07:29:18 +0000

    Pending changes exported from your codespace

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,3 +1,4 @@
+
 class Card {
   constructor(link, name) {
     this._name = name;
@@ -21,6 +22,7 @@ class Card {
     this._element.querySelector('.cards__image').src = this._image;
     this._element.querySelector('.cards__image').alt = this._name;
     this._element.querySelector('.cards__title').textContent = this._name;
+    
 
     //console.log(this._element)
     return this._element;
@@ -29,17 +31,29 @@ class Card {
     this._element.querySelector('.button_type_like').addEventListener('click', () => {
       this._handleLikeClick();
     });
-     this._element.querySelector('.button_type_delete').addEventListener('click', () => {
+    this._element.querySelector('.button_type_delete').addEventListener('click', () => {
       this._handleLikeDelete()
+    });
+    this._element.querySelector('.cards__image').addEventListener('click', () => {
+      this._openPopupImg();
+
     });
   }
   _handleLikeClick() {
     this._element.querySelector('.button_type_like').classList.toggle('button_type_like_active');
   }
-_handleLikeDelete() {
+  _handleLikeDelete() {
     this._element.querySelector('.button_type_delete').closest('.cards__item').remove();
   }
+  _openPopupImg() {
+    // this._element.querySelector('.figure__image').src = this._image;
+    // this._element.querySelector('.figure__image').alt = this._name;
+    // this._element.querySelector('.figure__info').textContent = this._name;
+    openPopup(popupImg);
+    popupImg.classList.add('popup__container_image-preview')//затемнения фона попапа с картинкой
+  }
 }
+
 
 initialCards.forEach((item) => {
   const card = new Card(item.link, item.name);
