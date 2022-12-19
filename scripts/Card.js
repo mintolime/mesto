83be174a37742894d9a333644f041361389d1259
mintolime <104442896+mintolime@users.noreply.github.com>
2022-12-19 10:32:31 +0000

    completed the last functionality on the cards

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,3 +1,4 @@
+
 class Card {
   constructor(link, name) {
     this._name = name;
@@ -25,21 +26,35 @@ class Card {
     //console.log(this._element)
     return this._element;
   }
+  
   _setEventListeners() {
     this._element.querySelector('.button_type_like').addEventListener('click', () => {
       this._handleLikeClick();
     });
-     this._element.querySelector('.button_type_delete').addEventListener('click', () => {
+    this._element.querySelector('.button_type_delete').addEventListener('click', () => {
       this._handleLikeDelete()
+    });
+    this._element.querySelector('.cards__image').addEventListener('click', () => {
+      this._openPopupImg();
+
     });
   }
   _handleLikeClick() {
     this._element.querySelector('.button_type_like').classList.toggle('button_type_like_active');
   }
-_handleLikeDelete() {
+  _handleLikeDelete() {
     this._element.querySelector('.button_type_delete').closest('.cards__item').remove();
   }
+  _openPopupImg() {
+    openPopup(popupImg);
+    console.log(popupImg)
+    document.querySelector('.figure__image').src = this._image
+    document.querySelector('.figure__info').textContent = this._name;
+    document.querySelector('.figure__info').alt = this._name;
+    popupImg.classList.add('popup__container_image-preview')//затемнения фона попапа с картинкой
+  }
 }
+
 
 initialCards.forEach((item) => {
   const card = new Card(item.link, item.name);
