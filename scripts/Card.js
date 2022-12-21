a0d82e0e9adae7a47e3b19cfac57c2370eaef1fa
Evgenia Smirnova <evgeniabright@yandex.ru>
2022-12-21 17:17:51 +0700

    connections of the development branch and the markup creation branch

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,4 +1,3 @@
-
 class Card {
   constructor(link, name) {
     this._name = name;
@@ -16,40 +15,36 @@ class Card {
 
 
   generateCard() {
-    this._element = this._getTemplate();
+    this._newCard = this._getTemplate();
     this._setEventListeners();
-
-    this._element.querySelector('.cards__image').src = this._image;
-    this._element.querySelector('.cards__image').alt = this._name;
-    this._element.querySelector('.cards__title').textContent = this._name;
-    
-
-    //console.log(this._element)
-    return this._element;
+    this._setData();
+    //console.log(this._newCard)
+    return this._newCard;
   }
-  _setEventListeners() {
-    this._element.querySelector('.button_type_like').addEventListener('click', () => {
-      this._handleLikeClick();
-    });
-    this._element.querySelector('.button_type_delete').addEventListener('click', () => {
-      this._handleLikeDelete()
-    });
-    this._element.querySelector('.cards__image').addEventListener('click', () => {
-      this._openPopupImg();
+  _setData() {
+    this._newCard.querySelector('.cards__image').src = this._image;
+    this._newCard.querySelector('.cards__image').alt = this._name;
+    this._newCard.querySelector('.cards__title').textContent = this._name;
+    document.querySelector('.figure__image').src = this._image
+    document.querySelector('.figure__info').textContent = this._name;
+    document.querySelector('.figure__info').alt = this._name; //нужно переместить в файл elements , либо сделать более лаконично! пока работает
+  }
 
-    });
+  _setEventListeners() {
+    this._newCard.querySelector('.button_type_like').addEventListener('click', () => this._handleLikeClick());
+    this._newCard.querySelector('.button_type_delete').addEventListener('click', () => this._handleLikeDelete());
+    this._newCard.querySelector('.cards__image').addEventListener('click', () => this._openPopupImg());
   }
   _handleLikeClick() {
-    this._element.querySelector('.button_type_like').classList.toggle('button_type_like_active');
+    this._newCard.querySelector('.button_type_like').classList.toggle('button_type_like_active');
   }
   _handleLikeDelete() {
-    this._element.querySelector('.button_type_delete').closest('.cards__item').remove();
+    this._newCard.remove();
+    this._newCard = null;
   }
   _openPopupImg() {
-    // this._element.querySelector('.figure__image').src = this._image;
-    // this._element.querySelector('.figure__image').alt = this._name;
-    // this._element.querySelector('.figure__info').textContent = this._name;
     openPopup(popupImg);
+    //console.log(popupImg)
     popupImg.classList.add('popup__container_image-preview')//затемнения фона попапа с картинкой
   }
 }
