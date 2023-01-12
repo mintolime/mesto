726633a53cf28d00b2091b47a99e7c8066ccb615
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-01-12 15:41:39 +0700

    setting HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 // webpack.config.js
 const path = require('path'); // подключаем path к конфигу вебпак
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плагин
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин
 
 module.exports = {
   entry: { main: './src/scripts/index.js' },
@@ -9,13 +11,32 @@ module.exports = {
     publicPath: ''
   },
   mode: 'development',
-   devServer: {
+  devServer: {
     static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
     compress: true, // это ускорит загрузку в режиме разработки
     port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
 
     open: true // сайт будет открываться сам при запуске npm run dev
   },
+  module: {
+    rules: [ // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        // регулярное выражение, которое ищет все js файлы
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        use: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: '/node_modules/'
+      }
+    ]
+  },
+  plugins: [
+  new HtmlWebpackPlugin({
+    template: './src/index.html' // путь к файлу index.html
+  }),
+   new CleanWebpackPlugin(), // использовали плагин
+]
 }
 
 // переписали точку выхода, используя утилиту path
