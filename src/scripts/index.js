700ef24f205f4338caf46edd4732ba58b9caafb5
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-01-13 20:58:00 +0700

    go merge

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import '../pages/index.css';
+import Section from './Section.js'
 import Card from './Card.js'
 import FormValidator from './FormValidator.js'
 import { initialCards } from './data-card.js'
@@ -22,17 +24,18 @@ import {
   popupProfileAddButton,
   popupProfileSaveButton,
 } from './constants.js'
+import Popup from './Popup.js'
 
 //универсальные функции
-function openPopup(popup) {
-  popup.classList.add('popup_opened')
-  document.addEventListener('keydown', closePopupByEsc);
-};
+// function openPopup(popup) {
+  // popup.classList.add('popup_opened')
+  // document.addEventListener('keydown', closePopupByEsc);
+// };
 
-function closePopup(popup) {
-  popup.classList.remove('popup_opened')
-  document.removeEventListener('keydown', closePopupByEsc);
-};
+// function closePopup(popup) {
+//   popup.classList.remove('popup_opened')
+//   document.removeEventListener('keydown', closePopupByEsc);
+// };
 
 //закрытие попапа по кнопке Esc
 function closePopupByEsc(evt) {
@@ -56,21 +59,21 @@ function changeTextProfile(evt) {
 };
 
 //закрытие попапов
-popups.forEach((popup) => {
-  popup.addEventListener('click', function (evt) {
-    if (evt.target.classList.contains('popup') || evt.target.classList.contains('button_type_close')) {
-      closePopup(popup);
-    }
-  });
-});
+// popups.forEach((popup) => {
+//   popup.addEventListener('click', function (evt) {
+//     if (evt.target.classList.contains('popup') || evt.target.classList.contains('button_type_close')) {
+//       closePopup(popup);
+//     }
+//   });
+// });
 
 // создание карточек в разметке
-function createCard(item, templateSelector, imgFigure, infoFigure, openImg) {
-  const card = new Card(item, templateSelector, imgFigure, infoFigure, openImg);
-  const cardElement = card.generateCard();
+// function createCard(item, templateSelector, imgFigure, infoFigure, openImg) {
+//   const card = new Card(item, templateSelector, imgFigure, infoFigure, openImg);
+//   const cardElement = card.generateCard();
 
-  cardContainer.prepend(cardElement);
-};
+//   cardContainer.prepend(cardElement);
+// };
 
 //открытие попапа с картинкой
 function openImg(name, img) {
@@ -81,20 +84,33 @@ function openImg(name, img) {
   infoFigure.textContent = name;
 };
 
+const newpopupCard = new Popup()
+
 //добавление карточки
 const submitCardAdd = (evt) => {
   evt.preventDefault();
-  createCard({
-    name: cardInputName.value,
-    link: cardImgLink.value
-  },
-    '#card-template',
-    imgFigure,
-    infoFigure,
-    openImg);
+  // createCard({
+  //   name: cardInputName.value,
+  //   link: cardImgLink.value
+  // },
+  //   '#card-template',
+  //   imgFigure,
+  //   infoFigure,
+  //   openImg);
   formCard.reset()
   closePopup(popupCard)
 };
+
+//класс вставки разметки класса Card
+const sectionCard = new Section({
+  items: initialCards,
+  renderer: (item) => {
+    const card = new Card(item, '#card-template', imgFigure, infoFigure, openImg);
+    const cardElement = card.generateCard();
+    sectionCard.addItem(cardElement)
+  }
+}, cardContainer);
+sectionCard.renderItems();
 
 //создания экзепмляра форм
 const validationFormPopupEdit = new FormValidator(formProfile, validationConfig);
@@ -104,7 +120,7 @@ const validationFormPopupAdd = new FormValidator(formCard, validationConfig);
 validationFormPopupAdd.enableValidation();
 
 //отрисовка всех карточек
-initialCards.forEach((item) => {createCard(item, '#card-template', imgFigure, infoFigure, openImg)});
+//initialCards.forEach((item) => { createCard(item, '#card-template', imgFigure, infoFigure, openImg) });
 
 //обработчики событий
 popupProfileAddButton.addEventListener('click', () => {
