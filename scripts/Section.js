41eef1380ce25248d01e9f2f53dbcc9c0bc6d736
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-01-13 15:36:43 +0700

    create Section.class (2)

--- scripts/Section.js
+++ scripts/Section.js
@@ -4,17 +4,16 @@ export default class Section {
     this._renderer = renderer;
     this._container = document.querySelector(containerSelector);
   }
+
   addItem(element) {
     this._container.append(element)
   }
 
-  // clear() {
-  //   this._container.innerHTML = '';
-  // }
-  renderer() {
-    this._initialArray.forEach((item) => {
-
+  renderItems() {
+    this._initialArray.forEach(item => {
+      this._renderer(item);
     });
   }
+
 }
 
