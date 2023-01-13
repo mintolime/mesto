51a43fbb9bf1b9da4684699aa5c3be24c7f4409e
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-01-13 11:58:18 +0700

    add file  postcss.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -42,8 +42,13 @@ module.exports = {
         // при обработке этих файлов нужно использовать
         // MiniCssExtractPlugin.loader и css-loader
         use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader'
-        }]
+          loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+        },
+        'postcss-loader'
+        ]
       },
     ]
   },
