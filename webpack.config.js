c77e1d5cf3e8b88c12f70763548e8c11a81c599b
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-01-14 19:49:34 +0700

    install devtool: "eval-source-map"

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
     publicPath: ''
   },
   mode: 'development',
+  devtool: "eval-source-map",
   devServer: {
     static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
     compress: true, // это ускорит загрузку в режиме разработки
