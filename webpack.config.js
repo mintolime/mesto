b45e658dbc49273b34d81773419d23434efa0502
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-02-12 12:40:05 +0700

    feat: add online html webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,10 @@ module.exports = {
   devtool: 'eval-source-map',
   devServer: {
     static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    watchFiles: ['src/*.html'],
+    hot: true,
     compress: true, // это ускорит загрузку в режиме разработки
     port: 8081, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-
     open: true // сайт будет открываться сам при запуске npm run dev
   },
   module: {
